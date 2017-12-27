aaf647d7a2cbe43451799eeb4c697f8e2a4a0288
soblin <hilo.mamoru@gmail.com>
2017-12-27 18:22:12 +0900

    12/27/18:22

---
+++ cuMat/CMakeFiles/a.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a.out.dir/a.out_generated_add.cu.o"
+  "a.out.pdb"
+  "a.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/a.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cuMat/CMakeFiles/add.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/add.dir/add_generated_add.cu.o"
+  "add.pdb"
+  "add"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/add.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
